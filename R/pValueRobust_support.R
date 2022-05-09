43f776a0bc0796b1f0e8827c431bd45b2c190b9d
LACourtenay <ladc1995@gmail.com>
2022-05-09 13:47:24 +0200

    pValueRobust_support.R inclusion

---
+++ R/pValueRobust_support.R
@@ -0,0 +1,16 @@
+
+#' @name pValueRobust
+#' @docType package
+#' @aliases pValueRobust
+#' @title A Library for Robust Statistical Measures and the Evaluation of p-Values
+#' @author Lloyd A. Courtenay
+#' @description A small toolkit for the evaluation of p-Values in statistical
+#' analyses, and some functions for the calculation of robust statistical
+#' measurements.
+#'
+#' @import grDevices
+#' @import graphics
+#' @import stats
+#' @import utils
+#'
+NULL
